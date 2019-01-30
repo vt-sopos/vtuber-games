a463f17d33f906cd53b09936e443bdeeed2e80e2
vt-sopos <vt_sopos@yahoo.co.jp>
2019-01-30 17:40:10 +0900

    '画面サイズ設定'

--- pages/rts/game.js
+++ pages/rts/game.js
@@ -2,15 +2,45 @@
 
 document.addEventListener('DOMContentLoaded', function() {
 
-//キャンバス作成
+//Canvas 作成
 const canvas = document.createElement('canvas');
 canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
-
 document.body.appendChild(canvas);
 
-//WebGLコンテキストの取得
 const gl = canvas.getContext('webgl2');
+
+//element, string
+function fireEvent(element, event) {
+    let evt = document.createEvent('HTMLEvents');
+    evt.initEvent(event, true, true);
+    return element.dispatchEvent(evt);
+}
+
+window.addEventListener('resize', function() {
+    canvas.width = window.innerWidth;
+    canvas.height = window.innerHeight;
+
+    //16:9にする
+    let x = 0;
+    let y = 0;
+    let width = window.innerWidth
+    let height = window.innerHeight;
+
+    if(window.innerWidth / 16 >= window.innerHeight / 9) {
+        width = (window.innerHeight / 9) * 16;
+        x = (window.innerWidth - width) / 2;
+    } else {
+        height = (window.innerWidth / 16) * 9;
+        y = (window.innerHeight - height) / 2;
+    }
+
+    gl.viewport(x, y, width, height);
+
+    console.log('resize');
+});
+
+fireEvent(window, 'resize');
 
 //array, number
 function createShader(source, type) {
@@ -25,7 +55,6 @@ function createShader(source, type) {
 
     return shader;
 }
-
 //number, number
 function createProgram(vertexShader, fragmentShader) {
     const program = gl.createProgram();
@@ -40,12 +69,24 @@ function createProgram(vertexShader, fragmentShader) {
 
     return program;
 }
+//number, webGLBuffer
+function createBuffer(type, array) {
+    const buffer = gl.createBuffer();
+    gl.bindBuffer(type, buffer);
+    gl.bufferData(type, array, gl.STATIC_DRAW);
+    gl.bindBuffer(type, null);
+    
+    return buffer;
+}
 
 Promise.all([
     fetch('glsl/vertex.glsl'), fetch('glsl/fragment.glsl')
 ]).then(responses => Promise.all([
     responses[0].text(), responses[1].text()
 ])).then(sources => {
+    //
+    //  プログラム作成
+    //
     const program = createProgram(
         createShader([sources[0]], gl.VERTEX_SHADER),
         createShader([sources[1]], gl.FRAGMENT_SHADER)
@@ -53,9 +94,40 @@ Promise.all([
     
     gl.useProgram(program);
 
-    //データ転送
-    const vertexBuffer = gl.createBuffer();
-    const indexBuffer = gl.createBuffer();
+    //
+    //  設定の有効化
+    //
+    gl.enable(gl.DEPTH_TEST);
+    gl.enable(gl.CULL_FACE);
+
+    //
+    //  uniform変数の設定
+    //
+
+    //
+    //  描画データ
+    //
+    const vertices = new Float32Array([
+        -1.0,1.0, 0.0,
+        1.0, 0.0, 0.0, 1.0,
+        -1.0,-1.0,0.0,
+        0.0, 1.0, 0.0, 1.0,
+        1.0, 1.0, 0.0,
+        0.0, 0.0, 1.0, 1.0,
+        1.0,-1.0, 0.0,
+        0.0, 0.0, 0.0, 1.0
+    ]);
+
+    const indices = new Uint16Array([
+        0, 1, 2,
+        1, 3, 2
+    ]);
+
+    //
+    //  バッファの設定
+    //
+    const vertexBuffer = createBuffer(gl.ARRAY_BUFFER, vertices);
+    const indexBuffer = createBuffer(gl.ELEMENT_ARRAY_BUFFER, indices);
 
     const VERTEX_ATTRIB_LOCATION = 0;
     const COLOR_ATTRIB_LOCATION = 1;
@@ -68,39 +140,26 @@ Promise.all([
     const COLOR_OFFSET = VERTEX_SIZE * Float32Array.BYTES_PER_ELEMENT;
 
     gl.bindBuffer(gl.ARRAY_BUFFER, vertexBuffer);
+
     gl.enableVertexAttribArray(VERTEX_ATTRIB_LOCATION);
     gl.enableVertexAttribArray(COLOR_ATTRIB_LOCATION);
 
     gl.vertexAttribPointer(VERTEX_ATTRIB_LOCATION, VERTEX_SIZE, gl.FLOAT, false, STRIDE, POSITION_OFFSET);
     gl.vertexAttribPointer(COLOR_ATTRIB_LOCATION, COLOR_SIZE, gl.FLOAT, false, STRIDE, COLOR_OFFSET);
-
-    const vertices = new Float32Array([
-        -0.5, 0.5,  0.0,
-        1.0, 0.0, 0.0, 1.0,
-        -0.5, -0.5, 0.0,
-        0.0, 1.0, 0.0, 1.0,
-        0.5,  0.5,  0.0,
-        0.0, 0.0, 1.0, 1.0,
-        0.5,  -0.5, 0.0,
-        0.0, 0.0, 0.0, 1.0
-    ]);
-
-    const indices = new Uint16Array([
-        0, 1, 2,
-        1, 3, 2
-    ]);
-
-    gl.bindBuffer(gl.ARRAY_BUFFER, vertexBuffer);
-    gl.bufferData(gl.ARRAY_BUFFER, vertices, gl.STATIC_DRAW);
     
-    gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, indexBuffer);
-    gl.bufferData(gl.ELEMENT_ARRAY_BUFFER, indices, gl.STATIC_DRAW);
+    function loop(timestamp) {
+        //
+        //  描画
+        //
+        gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
+        gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, indexBuffer);
+        gl.drawElements(gl.TRIANGLES, indices.length, gl.UNSIGNED_SHORT, 0);
+        gl.flush();
 
-    const VERTEX_NUMS = 6;
-
-    gl.drawElements(gl.TRIANGLES, indices.length, gl.UNSIGNED_SHORT, 0);
-
-    gl.flush();
+        window.requestAnimationFrame(loop);
+    }
+    
+    window.requestAnimationFrame(loop);
 });
 
 });
---
+++ pages/rts/math.js
@@ -0,0 +1,86 @@
+'use strict';
+
+class vec2 {
+    constructor() {
+        this.x = 0;
+        this.y = 0;
+    }
+
+    get array() {
+        return new Float32Array([x, y]);
+    }
+}
+
+class mat3 {
+    constructor() {
+        this.array = new Float32Array(9);
+    }
+}
+
+mat3.prototype.init = function() {
+    this.array[0] = 1; this.array[3] = 0; this.array[6] = 0;
+    this.array[1] = 0; this.array[4] = 1; this.array[7] = 0;
+    this.array[2] = 0; this.array[5] = 0; this.array[8] = 1;
+
+    return this;
+}
+
+mat3.prototype.identity = mat3.init;
+
+//Float32Array
+mat3.prototype.init = function(array) {
+    this.array = array;
+
+    return this;
+}
+
+//float...
+mat3.prototype.init = function(m0, m1, m2, m3, m4, m5, m6, m7, m8) {
+    this.array[0] = m0; this.array[3] = m1; this.array[6] = m2;
+    this.array[1] = m3; this.array[4] = m4; this.array[7] = m5;
+    this.array[2] = m6; this.array[5] = m7; this.array[8] = m8;
+
+    return this;
+}
+
+//mat3
+mat3.prototype.multiply = function(m) {
+    return new mat3().init(
+        this.array[0] * m.array[0] + this.array[3] * m.array[1] + this.array[6] * m.array[2],
+        this.array[0] * m.array[3] + this.array[3] * m.array[4] + this.array[6] * m.array[5],
+        this.array[0] * m.array[6] + this.array[3] * m.array[7] + this.array[6] * m.array[8],
+        this.array[1] * m.array[0] + this.array[4] * m.array[1] + this.array[7] * m.array[2],
+        this.array[1] * m.array[3] + this.array[4] * m.array[4] + this.array[7] * m.array[5],
+        this.array[1] * m.array[6] + this.array[4] * m.array[7] + this.array[7] * m.array[8],
+        this.array[2] * m.array[0] + this.array[5] * m.array[1] + this.array[8] * m.array[2],
+        this.array[2] * m.array[3] + this.array[5] * m.array[4] + this.array[8] * m.array[5],
+        this.array[2] * m.array[6] + this.array[5] * m.array[7] + this.array[8] * m.array[8]
+    );
+}
+
+//vec2
+mat3.prototype.translate = function(v) {
+    this.array[6] = v.x;
+    this.array[7] = v.y;
+
+    return this;
+}
+
+//vec2
+mat3.prototype.scale = function(v) {
+    this.array[0] = v.x;
+    this.array[4] = v.y;
+
+    return this;
+}
+
+//float
+mat3.prototype.rotate = function(f) {
+    const cf = Math.cos(f);
+    const sf = Math.sin(f);
+
+    this.array[0] = cf; this.array[3] = -sf;
+    this.array[1] = sf; this.array[4] = cf;
+
+    return this;
+}
